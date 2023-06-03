c385626bd343b1280c1eb434ff4ff8ff27c493e9
dlawjddn <jwl5015@naver.com>
2023-06-03 21:22:11 +0900

    develop chown

--- osproject.c
+++ osproject.c
@@ -1011,6 +1011,58 @@ void ChangeOwnerAll(TreeNode* NodeDir, char* userName)
     NodeDir->UID = tmpUser->UID;
     NodeDir->GID = tmpUser->GID;
 }
+/*
+int chown_(DirectoryTree* dirTree, char* cmd)      //완료
+{
+    DirectoryNode* tmpNode = NULL;
+    UserNode* tmpUser = NULL;
+    char* str;
+    char tmp[MAX_NAME];
+
+    if(cmd == NULL){
+        printf("Try 'chown --help' for more information.\n");
+        return -1;
+    }
+    if(cmd[0] == '-'){
+        if(strcmp(cmd, "--help") == 0){
+            printf("사용법: chown [옵션]... [소유자]... 파일...\n");
+            printf("  Change the owner and/or group of each FILE to OWNER and/or GROUP.\n\n");
+            printf("  Options:\n");
+            printf("    -R, --recursive\t change files and directories recursively\n");
+            printf("        --help\t 이 도움말을 표시하고 끝냅니다\n");
+        }
+        return -1;
+    }
+    else{
+        strncpy(tmp, cmd, MAX_NAME);
+        str = strtok(NULL, " ");
+        if(str == NULL){
+            printf("Try 'chown --help' for more information.\n");
+            return -1;
+        }
+        else{
+            //그룹
+            if (strstr(tmp, ":") == NULL){
+                ChangeOwner(dirTree, tmp, str, 0);
+            }
+            else{
+                char tmp2[MAX_NAME];
+                strncpy(tmp2, tmp, MAX_NAME);
+                char *str2 = strtok(tmp, ":");
+                if (str2 != NULL && strcmp(tmp, tmp2) != 0){
+                    ChangeOwner(dirTree, str2, str, 0);
+                    str2 = strtok(NULL, " ");
+                    if (str2 != NULL)
+                        ChangeOwner(dirTree, str2, str, 1);
+                }
+                else if (str2 != NULL && strcmp(tmp, tmp2) == 0)
+                    ChangeOwner(dirTree, str2, str, 1);
+            }
+        }
+    }
+    return 0;
+}
+*/
 int chown_(DirectoryTree* dirTree, char* cmd)
 {
     TreeNode* tmpNode = NULL;
